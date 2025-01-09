498ed8c38ce4f44f105699f61ee7b2064839fc3b
chrisza4 <chakrit.lj@gmail.com>
2025-01-09 15:51:19 +0700

    Add rmv implementation

--- src/simulator/params.rs
+++ src/simulator/params.rs
@@ -2,12 +2,20 @@
 pub enum Rmv {
     Register(String),
     Value(u16),
-    Memory(u16),
+    Memory(String),
 }
 
 impl Rmv {
-    fn from_str(src: &str) -> Rmv {
-        Rmv::Register(src.to_string())
+    pub fn from_str(src: &str) -> Rmv {
+        let binding = src.replace("word", "");
+        let sanitized_src = binding.trim();
+        if sanitized_src.starts_with("[") {
+            return Rmv::Memory(sanitized_src.trim_start_matches("[").trim_end_matches("]").to_string());
+        }
+        match sanitized_src.parse::<u16>() {
+            Ok(u) => Rmv::Value(u),
+            Err(_) => Rmv::Register(src.to_string()),
+        }
     }
 }
 
@@ -18,5 +26,24 @@ mod tests {
     #[test]
     fn test_rmv_from_register() {
         assert_eq!(Rmv::Register("ax".to_string()), Rmv::from_str("ax"));
+        assert_eq!(Rmv::Register("bx".to_string()), Rmv::from_str("bx"));
+    }
+
+    #[test]
+    fn test_rmv_from_value() {
+        assert_eq!(Rmv::Value(1000), Rmv::from_str("1000"));
+        assert_eq!(Rmv::Value(20), Rmv::from_str("20"));
+    }
+
+    #[test]
+    fn test_rmv_from_memory() {
+        assert_eq!(Rmv::Memory("bp + si".to_string()), Rmv::from_str("[bp + si]"));
+        assert_eq!(Rmv::Memory("2012".to_string()), Rmv::from_str("[2012"));
+    }
+
+    #[test]
+    fn test_rmv_from_dirty_string() {
+        assert_eq!(Rmv::Memory("bp + si".to_string()), Rmv::from_str("word [bp + si]"));
+        assert_eq!(Rmv::Value(1000), Rmv::from_str("  1000"));
     }
 }
